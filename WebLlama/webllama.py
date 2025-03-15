32e3bcf9452ea20cb16ff39fbee5d0d6ed9b518a
Hawk3388 <arvid.bouziane@icloud.com>
2025-03-15 23:59:00 +0100

    modified:   WebLlama/webllama.py

--- WebLlama/webllama.py
+++ WebLlama/webllama.py
@@ -365,7 +365,7 @@ Task: Determine whether additional context from internet sources is required to 
                 emb = OllamaEmbeddings(model=self.embeddings)
                 emb.embed_query("test")
             except ollama.ResponseError:
-                print(f"Error: model '{self.embeddings}' not found\n")
+                print(f"Error: model '{self.embeddings}' not found")
                 sys.exit()
         try:
             ChatOllama(model=self.model, num_ctx=self.num_ctx, format=self.Websearch.model_json_schema(), verbose=self.verbose, seed=self.seed, num_predict=self.predict, top_k=self.top_k, top_p=self.top_p, temperature=self.temperature, repeat_penalty=self.repeat_penalty, repeat_last_n=self.repeat_last_n, num_gpu=self.num_gpu, stop=self.stop, keep_alive=self.keep_alive).invoke("test")
@@ -374,7 +374,7 @@ Task: Determine whether additional context from internet sources is required to 
                 ollama.pull(self.model)
                 ChatOllama(model=self.model, num_ctx=self.num_ctx, format=self.Websearch.model_json_schema(), verbose=self.verbose, seed=self.seed, num_predict=self.predict, top_k=self.top_k, top_p=self.top_p, temperature=self.temperature, repeat_penalty=self.repeat_penalty, repeat_last_n=self.repeat_last_n, num_gpu=self.num_gpu, stop=self.stop, keep_alive=self.keep_alive).invoke("test")
             except ollama.ResponseError:
-                print(f"Error: model '{self.model}' not found\n")
+                print(f"Error: model '{self.model}' not found")
                 sys.exit()
 
     def multiline_input(self, prompt=">>> "):
